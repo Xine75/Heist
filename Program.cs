b78298f3066cdd1fac2a01f0041f7592be9e2395
Xine <cjdoza@gmail.com>
2021-06-01 14:24:21 -0500

    trial runs

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace Heist
         static void Main(string[] args)
         {
             Console.WriteLine("Plan Your Heist!");
+
             Console.Write("Choose a conspirator...\n>");
             string conspirator = Console.ReadLine();
 
@@ -17,61 +18,70 @@ namespace Heist
             var luckValue = random.Next(-10, 10);
             int bankDifficultyLevel = 100 + luckValue;
 
+            Console.WriteLine("How many times would you like to run your heist?");
+            int trialRuns = Int32.Parse(Console.ReadLine());
 
             List<Conspirator> Team = new List<Conspirator>();
 
-            MakeYourTeam();
-            void MakeYourTeam()
+            while (trialRuns > 0)
             {
-                if (conspirator == "")
+
+                MakeYourTeam();
+
+                void MakeYourTeam()
                 {
-
-                    Console.WriteLine("Your team is complete!");
-                    int teamCount = Team.Count;
-
-                    Console.WriteLine($"Your team has {teamCount} criminal masterminds.");
-
-                    foreach (Conspirator criminal in Team)
+                    if (conspirator == "")
                     {
-                        Console.WriteLine($"{criminal.Name} has a Skill Level of {criminal.SkillLevel} and courage level of {criminal.CourageFactor}.");
-                        teamSkillLevel += criminal.SkillLevel;
-                    }
 
-                    Console.WriteLine($"Team Skill Level: {teamSkillLevel}");
-                    Console.WriteLine($"Bank Difficulty Level: {bankDifficultyLevel}");
+                        Console.WriteLine("Your team is complete!");
+                        int teamCount = Team.Count;
 
-                    if (teamSkillLevel >= bankDifficultyLevel)
-                    {
-                        Console.WriteLine("Your heist is a success!!");
+                        Console.WriteLine($"Your team has {teamCount} criminal masterminds.");
+
+                        foreach (Conspirator criminal in Team)
+                        {
+                            Console.WriteLine($"{criminal.Name} has a Skill Level of {criminal.SkillLevel} and courage level of {criminal.CourageFactor}.");
+                            teamSkillLevel += criminal.SkillLevel;
+                        }
+
+                        Console.WriteLine($"Team Skill Level: {teamSkillLevel}");
+                        Console.WriteLine($"Bank Difficulty Level: {bankDifficultyLevel}");
+                        trialRuns--;
+
+                        if (teamSkillLevel >= bankDifficultyLevel)
+                        {
+                            Console.WriteLine("Your heist is a success!!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Your heist is a failure!");
+                        }
+
+                        //LINQ method
+                        // teamSkillLevel = Team.Sum(x => x.SkillLevel);
+                        return;
                     }
                     else
                     {
-                        Console.WriteLine("Your heist is a failure!");
+                        Console.WriteLine($"You have chosen {conspirator}");
+
+                        Console.Write($"On a scale of 1 - 10, what is {conspirator}'s Skill Level?\n>");
+                        int skill = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine($"{conspirator}'s skill level is {skill}.  Very good.");
+
+                        Console.Write($"From 0.0 to 2.0 , what is {conspirator}'s Courage Factor?\n>");
+                        double courage = Double.Parse(Console.ReadLine());
+                        //Console.WriteLine($"{conspirator}'s SKILL LEVEL is {skill} and COURAGE FACTOR IS {courage}.  You have chosen well.");
+
+                        Conspirator criminal = new Conspirator(conspirator, skill, courage);
+                        Team.Add(criminal);
+                        trialRuns--;
+
+                        Console.WriteLine("Choose your next teammate \n>");
+                        conspirator = Console.ReadLine();
+
+                        MakeYourTeam();
                     }
-
-                    //LINQ method
-                    // teamSkillLevel = Team.Sum(x => x.SkillLevel);
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine($"You have chosen {conspirator}");
-
-                    Console.Write($"On a scale of 1 - 10, what is {conspirator}'s Skill Level?\n>");
-                    int skill = Int32.Parse(Console.ReadLine());
-                    Console.WriteLine($"{conspirator}'s skill level is {skill}.  Very good.");
-
-                    Console.Write($"From 0.0 to 2.0 , what is {conspirator}'s Courage Factor?\n>");
-                    double courage = Double.Parse(Console.ReadLine());
-                    //Console.WriteLine($"{conspirator}'s SKILL LEVEL is {skill} and COURAGE FACTOR IS {courage}.  You have chosen well.");
-
-                    Conspirator criminal = new Conspirator(conspirator, skill, courage);
-                    Team.Add(criminal);
-
-                    Console.WriteLine("Choose your next teammate \n>");
-                    conspirator = Console.ReadLine();
-
-                    MakeYourTeam();
                 }
             }
         }
